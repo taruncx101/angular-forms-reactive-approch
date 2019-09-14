03c15aea83875140e068f107069d4b923eee9b45
Tarun Gorai <goraitarunkumar@gmail.com>
2019-09-14 09:22:49 +0530

    create a cutom validator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,12 @@ import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   signUpForm: FormGroup;
+  forBiddenUserNames = ['Chris', 'Anna'];
 
   ngOnInit() {
     this.signUpForm = new FormGroup({
       'userData': new FormGroup({
-        'username': new FormControl(null, Validators.required),
+        'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
         'email': new FormControl(null, [Validators.required, Validators.email]),
       }),
       'gender': new FormControl('male'),
@@ -26,5 +27,11 @@ export class AppComponent implements OnInit {
   onAddHobby() {
     const control = new FormControl(null, Validators.required);
     (<FormArray>this.signUpForm.get('hobbies')).push(control);
+  }
+  forbiddenNames(control: FormControl): {[s: string]: boolean} {
+    if (this.forBiddenUserNames.indexOf(control.value) > -1) {
+      return {'nameIsForbidden': true};
+    }
+    return null;
   }
 }
