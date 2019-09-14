d93375e57ace5d3bdb6d337594674b05af9b83ce
Tarun Gorai <goraitarunkumar@gmail.com>
2019-09-14 17:13:22 +0530

    get values on value change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,6 +21,17 @@ export class AppComponent implements OnInit {
       'gender': new FormControl('male'),
       'hobbies': new FormArray([])
     });
+
+    // this.signUpForm.valueChanges.subscribe(
+    //   (value) => {
+    //     console.log(value);
+    //   }
+    // );
+    this.signUpForm.statusChanges.subscribe(
+      (status) => {
+        console.log(status);
+      }
+    );
   }
   onSubmit() {
     console.log(this.signUpForm);
